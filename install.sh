9acf545979fb98bfdec53d05ce0c3774cdcfa59c
chu <they.know.who.is.the.strongest@gmail.com>
2016-05-12 00:57:38 +0000

    update install sh

--- install.sh
+++ install.sh
@@ -3,3 +3,14 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+
+mkdir ~/.vimswap
+mkdir ~/.vimbackup
+mkdir ~/.vimundo
+
+mkdir ~/.cache/dein
+
+mkdir ~/.vim
+mkdir ~/.vim/dein
+
+
