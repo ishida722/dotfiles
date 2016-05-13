ef68a385bf0fb8f0bb3dbeedca8a67f4d9790c84
chu <they.know.who.is.the.strongest@gmail.com>
2016-05-13 02:39:46 +0000

    add tmux conf

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 mkdir ~/.vimswap
 mkdir ~/.vimbackup
