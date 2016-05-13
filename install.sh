459b41a4d52ac839a9026be051a2a884f0c72a68
chu <they.know.who.is.the.strongest@gmail.com>
2016-05-13 02:43:19 +0000

    add dein install setting

--- install.sh
+++ install.sh
@@ -5,13 +5,16 @@ ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
+#vim dir
 mkdir ~/.vimswap
 mkdir ~/.vimbackup
 mkdir ~/.vimundo
 
+#dein vim
 mkdir ~/.cache/dein
-
 mkdir ~/.vim
 mkdir ~/.vim/dein
 
-
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_installer.sh
+sh ./dein_installer.sh ~/.vim/dein
+rm dein_installer.sh
